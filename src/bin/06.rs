80cddb055e6bc41006573807c75f63aafff0626b
FineFindus <63370021+FineFindus@users.noreply.github.com>
2022-12-06 18:26:58 +0100

    feat(day/6): re-write as better solution

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,38 +1,40 @@
 use itertools::Itertools;
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let bytes = input.as_bytes();
-
-    let mut items: Vec<u8> = Vec::with_capacity(5);
-    for (index, byte) in bytes.iter().enumerate() {
-        items.push(*byte);
-
-        if index.checked_sub(4).is_some() {
-            items.remove(0);
-        }
-
-        if items.len() == 4 &&
-            items.iter().unique().collect_vec().len() == 4 {
+    let mut accumulator: u64 = 0;
+    let masks = input
+        .trim()
+        .as_bytes()
+        .iter()
+        .map(|char| 1 << (*char as u32 - 'a' as u32))
+        .collect_vec();
+    for (index, mask) in masks.iter().enumerate() {
+        accumulator ^= mask;
+        if index >= 4 {
+            accumulator ^= masks[index - 4];
+            if accumulator.count_ones() == 4 {
                 return Some(index as u32 + 1);
+            }
         }
     }
     None
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let bytes = input.as_bytes();
-
-    let mut items: Vec<u8> = Vec::with_capacity(5);
-    for (index, byte) in bytes.iter().enumerate() {
-        items.push(*byte);
-
-        if index.checked_sub(14).is_some() {
-            items.remove(0);
-        }
-
-        if items.len() == 14 &&
-            items.iter().unique().collect_vec().len() == 14 {
+    let mut accumulator: u64 = 0;
+    let masks = input
+        .trim()
+        .as_bytes()
+        .iter()
+        .map(|char| 1 << (*char as u32 - 'a' as u32))
+        .collect_vec();
+    for (index, mask) in masks.iter().enumerate() {
+        accumulator ^= mask;
+        if index >= 14 {
+            accumulator ^= masks[index - 14];
+            if accumulator.count_ones() == 14 {
                 return Some(index as u32 + 1);
+            }
         }
     }
     None
