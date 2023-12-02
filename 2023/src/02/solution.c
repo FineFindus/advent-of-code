515abfcfa67515013d848e3adc2afffe543208fd
FineFindus <FineFindus@proton.me>
2023-12-02 20:49:25 +0100

    feat(2023): solve day02 part two

--- 2023/src/02/solution.c
+++ 2023/src/02/solution.c
@@ -61,16 +61,64 @@ part_one (char *input)
   return sum;
 }
 
+int
+minimum_colors (char *str)
+{
+  char *temp;
+
+  // skip 5 char for "Game "
+  str += 5;
+  strtok_r (str, ":", &temp);
+
+  int red = 0;
+  int green = 0;
+  int blue = 0;
+  char *set = strtok_r (NULL, ";", &temp);
+  do
+    {
+      if (set == NULL)
+        continue;
+      char *color = strtok_r (NULL, ",", &set);
+      do
+        {
+          int value = atoi (color);
+          while (isalpha (*color) == 0)
+            color++;
+
+          if (strcmp (color, "green") == 0 && value > green)
+            green = value;
+          else if (strcmp (color, "red") == 0 && value > red)
+            red = value;
+          else if (strcmp (color, "blue") == 0 && value > blue)
+            blue = value;
+        }
+      while ((color = strtok_r (NULL, ",", &set)) != NULL);
+    }
+  while ((set = strtok_r (NULL, ";", &temp)) != NULL);
+
+  return red * green * blue;
+}
+
 unsigned
 part_two (char *input)
 {
-  return 0;
+  // having nearly identical solving functions is pretty wasteful,
+  // but I don't want to spent time fixing it…
+  unsigned sum = 0;
+  char *line, *temp;
+  line = strtok_r (input, "\n", &temp);
+  do
+    {
+      sum += minimum_colors (line);
+    }
+  while ((line = strtok_r (NULL, "\n", &temp)) != NULL);
+  return sum;
 }
 
 int
 main (int argc, char *argv[])
 {
-  test (part_one, 8, part_two, 0);
+  test (part_one, 8, part_two, 2286);
   print_result (part_one);
   print_result (part_two);
   return EXIT_SUCCESS;
