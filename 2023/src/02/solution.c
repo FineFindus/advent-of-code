dd965ef6b0678cb91cfd2a386937018a5b8ac380
FineFindus <FineFindus@proton.me>
2023-12-02 18:59:24 +0100

    feat(2023): solve day02 part one

---
+++ 2023/src/02/solution.c
@@ -0,0 +1,77 @@
+#include "../shared/aoc.h"
+#include <assert.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int
+parse_set (char *str, int max_red, int max_green, int max_blue)
+{
+  char *buf, *temp;
+
+  // skip 5 char for "Game "
+  str += 5;
+  buf = strtok_r (str, ":", &temp);
+  int id = atoi (buf);
+
+  char *set = strtok_r (NULL, ";", &temp);
+  do
+    {
+      int red = 0;
+      int green = 0;
+      int blue = 0;
+
+      char *color = strtok_r (NULL, ",", &set);
+      do
+        {
+          int value = atoi (color);
+          while (isalpha (*color) == 0)
+            color++;
+
+          if (strcmp (color, "green") == 0 && value <= max_green)
+            green += value;
+          else if (strcmp (color, "red") == 0 && value <= max_red)
+            red += value;
+          else if (strcmp (color, "blue") == 0 && value <= max_blue)
+            blue += value;
+          else
+            return 0;
+        }
+      while ((color = strtok_r (NULL, ",", &set)) != NULL);
+    }
+  while ((set = strtok_r (NULL, ";", &temp)) != NULL);
+
+  return id;
+}
+
+unsigned
+part_one (char *input)
+{
+  unsigned sum = 0;
+  char *line, *temp;
+  line = strtok_r (input, "\n", &temp);
+  do
+    {
+      sum += parse_set (line, 12, 13, 14);
+    }
+  while ((line = strtok_r (NULL, "\n", &temp)) != NULL);
+  return sum;
+}
+
+unsigned
+part_two (char *input)
+{
+  return 0;
+}
+
+int
+main (int argc, char *argv[])
+{
+  test (part_one, 8, part_two, 0);
+  print_result (part_one);
+  print_result (part_two);
+  return EXIT_SUCCESS;
+}
