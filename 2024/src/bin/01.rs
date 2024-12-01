15a8dc9d0ac497ab2aa8ceb6ed9ab977226364dd
FineFindus <FineFindus@proton.me>
2024-12-01 08:15:02 +0100

    feat(01): solve part two

--- 2024/src/bin/01.rs
+++ 2024/src/bin/01.rs
@@ -19,7 +19,27 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut similarity_score = HashMap::new();
+    Some(
+        input
+            .lines()
+            .filter_map(|line| {
+                let (id, val) = line
+                    .split_whitespace()
+                    .filter_map(|n| n.parse::<u32>().ok())
+                    .collect_tuple::<(u32, u32)>()?;
+                let score: u32 = similarity_score.get(&val).copied().unwrap_or_default();
+                similarity_score.insert(val, score + 1);
+                Some(id)
+            })
+            .collect::<Vec<_>>()
+            .into_iter()
+            .map(|id| {
+                let score = similarity_score.get(&id).unwrap_or(&0);
+                id * score
+            })
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -35,6 +55,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
