a3a049ddb80eddb7e96ff429e3265cff7f8f575f
FineFindus <FineFindus@proton.me>
2024-12-02 09:37:21 +0100

    refactor(2024/02): iterate over input once

--- 2024/src/bin/02.rs
+++ 2024/src/bin/02.rs
@@ -3,23 +3,26 @@ use itertools::Itertools;
 advent_of_code::solution!(2);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    Some(
-        input
-            .lines()
-            .map(|line| {
-                line.split_whitespace()
-                    .filter_map(|v| v.parse::<u32>().ok())
-                    .collect_vec()
+    let safe_reports = input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .filter_map(|v| v.parse::<u32>().ok())
+                .collect_vec()
+        })
+        .filter(|reports| {
+            let sort_order = if reports[0] <= reports[1] && reports[1] <= reports[2] {
+                |a, b| a <= b
+            } else {
+                |a, b| a >= b
+            };
+            reports.iter().tuple_windows().all(|(a, b)| {
+                let diff = a.abs_diff(*b);
+                sort_order(a, b) && (1..=3).contains(&diff)
             })
-            .filter(|reports| reports.is_sorted() || reports.is_sorted_by(|a, b| b <= a))
-            .filter(|reports| {
-                reports.iter().tuple_windows().all(|(a, b)| {
-                    let diff = a.abs_diff(*b);
-                    (1..=3).contains(&diff)
-                })
-            })
-            .count() as u32,
-    )
+        })
+        .count() as u32;
+    Some(safe_reports)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
