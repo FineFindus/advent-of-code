4e3899f1801372b4149ca231b54f68d29ce17db6
FineFindus <FineFindus@proton.me>
2024-12-02 19:02:39 +0100

    feat(2024/02): solve part two

--- 2024/src/bin/02.rs
+++ 2024/src/bin/02.rs
@@ -2,6 +2,14 @@ use itertools::Itertools;
 
 advent_of_code::solution!(2);
 
+fn is_safe(reports: &[u32]) -> bool {
+    (reports.is_sorted() || reports.is_sorted_by(|a, b| b <= a))
+        && reports.iter().tuple_windows().all(|(a, b)| {
+            let diff = a.abs_diff(*b);
+            (1..=3).contains(&diff)
+        })
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
     let safe_reports = input
         .lines()
@@ -10,23 +18,32 @@ pub fn part_one(input: &str) -> Option<u32> {
                 .filter_map(|v| v.parse::<u32>().ok())
                 .collect_vec()
         })
-        .filter(|reports| {
-            let sort_order = if reports[0] <= reports[1] && reports[1] <= reports[2] {
-                |a, b| a <= b
-            } else {
-                |a, b| a >= b
-            };
-            reports.iter().tuple_windows().all(|(a, b)| {
-                let diff = a.abs_diff(*b);
-                sort_order(a, b) && (1..=3).contains(&diff)
-            })
-        })
+        .filter(|reports| is_safe(reports))
         .count() as u32;
     Some(safe_reports)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let safe_reports = input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .filter_map(|v| v.parse::<u32>().ok())
+                .collect_vec()
+        })
+        .filter(|reports| {
+            let mut reports = reports.clone();
+            for index in 0..reports.len() {
+                let problem_dampener = reports.remove(index);
+                if is_safe(&reports) {
+                    return true;
+                }
+                reports.insert(index, problem_dampener)
+            }
+            false
+        })
+        .count() as u32;
+    Some(safe_reports)
 }
 
 #[cfg(test)]
@@ -42,6 +59,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
