57dc3751beb98a70026227f30538088051c49881
FineFindus <FineFindus@proton.me>
2024-12-03 08:59:33 +0100

    feat(2024/03): solve part two

--- 2024/src/bin/03.rs
+++ 2024/src/bin/03.rs
@@ -2,18 +2,26 @@ use itertools::Itertools;
 
 advent_of_code::solution!(3);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let regex = regex::Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").expect("`regex` should be valid");
-    regex
-        .captures_iter(input)
+fn execute_mul_instructions(instructions: &str) -> Option<u32> {
+    regex::Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)")
+        .expect("`regex` should be valid")
+        .captures_iter(instructions)
         .map(|c| c.extract())
         .map(|(_, [x, y])| (x.parse::<u32>().unwrap(), y.parse::<u32>().unwrap()))
         .map(|(x, y)| x * y)
         .sum1()
 }
 
+pub fn part_one(input: &str) -> Option<u32> {
+    execute_mul_instructions(input)
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    input
+        .split("do()")
+        .filter_map(|instructions| instructions.split("don't()").next())
+        .filter_map(execute_mul_instructions)
+        .sum1()
 }
 
 #[cfg(test)]
