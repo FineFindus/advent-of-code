11ad8285d97d30c021221d8fd45e249c42a7f890
FineFindus <FineFindus@proton.me>
2024-12-04 18:04:49 +0100

    feat(2024/04): solve part two

--- 2024/src/bin/04.rs
+++ 2024/src/bin/04.rs
@@ -1,4 +1,3 @@
-
 use itertools::Itertools;
 
 advent_of_code::solution!(4);
@@ -37,7 +36,39 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let grid = input
+        .lines()
+        .map(|line| line.chars().collect_vec())
+        .collect_vec();
+
+    let mut sum = 0;
+    for y in 0..grid.len() {
+        for x in 0..grid[y].len() {
+            if grid[y][x] != 'A' {
+                continue;
+            }
+
+            let elemns = (-1..=1)
+                .cartesian_product(-1..=1)
+                .filter(|(a, b)| *a != 0 && *b != 0)
+                .map(|(a, b)| (y as i32 + a, x as i32 + b))
+                .filter(|(new_x, new_y)| {
+                    (0..grid.len() as i32).contains(new_y)
+                        && (0..grid[y].len() as i32).contains(new_x)
+                })
+                .map(|(y, x)| grid[y as usize][x as usize])
+                .counts();
+
+            if elemns.get(&'M') == Some(&2)
+                && elemns.get(&'S') == Some(&2)
+            // extremly hacky solution
+                && grid[y - 1][x - 1] != grid[y + 1][x + 1]
+            {
+                sum += 1;
+            }
+        }
+    }
+    Some(sum)
 }
 
 #[cfg(test)]
@@ -53,6 +84,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(9));
     }
 }
