1d0b7257d4364ad4c808d6d9f0a398da0985980b
FineFindus <FineFindus@proton.me>
2024-12-05 18:15:24 +0100

    feat(2024/05): solve part two

--- 2024/src/bin/05.rs
+++ 2024/src/bin/05.rs
@@ -1,10 +1,10 @@
-use std::collections::{HashMap, HashSet};
+use std::{cmp, collections::HashSet};
 
 use itertools::Itertools;
 
 advent_of_code::solution!(5);
 
-pub fn part_one(input: &str) -> Option<u32> {
+fn parse_input(input: &str) -> Option<(HashSet<(u32, u32)>, impl Iterator<Item = Vec<u32>> + '_)> {
     let (rules, updates) = input.split_once("\n\n")?;
     let rules = rules
         .lines()
@@ -14,21 +14,41 @@ pub fn part_one(input: &str) -> Option<u32> {
         })
         .collect::<HashSet<_>>();
 
+    let updates = updates.lines().map(|line| {
+        line.split(",")
+            .map(|num| num.parse::<u32>().unwrap())
+            .collect_vec()
+    });
+    Some((rules, updates))
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (rules, updates) = parse_input(input)?;
     updates
-        .lines()
-        .map(|line| {
-            line.split(",")
-                .map(|num| num.parse::<u32>().unwrap())
-                .collect_vec()
-        })
         .filter(|update| update.is_sorted_by(|&a, &b| rules.contains(&(a, b))))
-        .inspect(|update| println!("{:?}", update))
         .map(|update| update[update.len() / 2])
         .sum1()
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (rules, updates) = parse_input(input)?;
+
+    updates
+        .filter(|update| !update.is_sorted_by(|&a, &b| rules.contains(&(a, b))))
+        .map(|mut update| {
+            update.sort_unstable_by(|&a, &b| {
+                if rules.contains(&(a, b)) {
+                    cmp::Ordering::Less
+                } else if rules.contains(&(b, a)) {
+                    cmp::Ordering::Greater
+                } else {
+                    cmp::Ordering::Equal
+                }
+            });
+            update
+        })
+        .map(|update| update[update.len() / 2])
+        .sum1()
 }
 
 #[cfg(test)]
@@ -44,6 +64,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(123));
     }
 }
