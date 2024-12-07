eeb181711e28f76283e7b8f5210b914ce30af6c3
FineFindus <FineFindus@proton.me>
2024-12-07 09:52:36 +0100

    feat(2024/07): solve part two

--- 2024/src/bin/07.rs
+++ 2024/src/bin/07.rs
@@ -2,12 +2,23 @@ use itertools::Itertools;
 
 advent_of_code::solution!(7);
 
-fn is_result_possible(result: u64, intermediate_result: u64, values: &[u64]) -> bool {
+fn is_result_possible(result: u64, intermediate_result: u64, values: &[u64], concat: bool) -> bool {
     let Some(v) = values.first() else {
         return intermediate_result == result;
     };
-    is_result_possible(result, intermediate_result + v, &values[1..])
-        || is_result_possible(result, intermediate_result * v, &values[1..])
+    is_result_possible(result, intermediate_result + v, &values[1..], concat)
+        || is_result_possible(result, intermediate_result * v, &values[1..], concat)
+        || (concat
+            && is_result_possible(
+                result,
+                concat_nums(intermediate_result, v),
+                &values[1..],
+                concat,
+            ))
+}
+
+fn concat_nums(a: u64, b: &u64) -> u64 {
+    format!("{a}{b}").parse::<u64>().unwrap()
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
@@ -23,13 +34,27 @@ pub fn part_one(input: &str) -> Option<u64> {
                     .collect_vec(),
             ))
         })
-        .filter(|(result, values)| is_result_possible(*result, values[0], &values[1..]))
+        .filter(|(result, values)| is_result_possible(*result, values[0], &values[1..], false))
         .map(|(result, _values)| result)
         .sum1()
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    input
+        .lines()
+        .filter_map(|line| line.split_once(":"))
+        .filter_map(|(result, values)| {
+            Some((
+                result.parse::<u64>().ok()?,
+                values
+                    .split_whitespace()
+                    .map(|val| val.parse::<u64>().unwrap())
+                    .collect_vec(),
+            ))
+        })
+        .filter(|(result, values)| is_result_possible(*result, values[0], &values[1..], true))
+        .map(|(result, _values)| result)
+        .sum1()
 }
 
 #[cfg(test)]
@@ -45,6 +70,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11387));
     }
 }
