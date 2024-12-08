b25b45d2ab48345a184c378257912177904991ab
FineFindus <FineFindus@proton.me>
2024-12-08 12:45:26 +0100

    feat(2024/08): solve part two

--- 2024/src/bin/08.rs
+++ 2024/src/bin/08.rs
@@ -1,5 +1,7 @@
 use std::collections::{HashMap, HashSet};
 
+use itertools::Itertools;
+
 advent_of_code::solution!(8);
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -42,7 +44,48 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let width = input.find("\n")?;
+    let height = input.split("\n").count();
+
+    let mut antenna: HashMap<char, Vec<(isize, isize)>> = HashMap::new();
+    input.lines().enumerate().for_each(|(line_index, line)| {
+        line.chars()
+            .enumerate()
+            .filter(|&(_, char)| char != '.')
+            .for_each(|(index, char)| {
+                let location = (index as isize, line_index as isize);
+                antenna
+                    .entry(char)
+                    .and_modify(|locs| locs.push(location))
+                    .or_insert(vec![location]);
+            })
+    });
+    let mut antinodes: HashSet<(isize, isize)> = HashSet::new();
+    let is_in_bounds = |x, y| (0..width as isize).contains(&y) && (0..height as isize).contains(&x);
+
+    for (_type, locations) in antenna {
+        for ((x, y), (x_2, y_2)) in locations.iter().tuple_combinations() {
+            if (x, y) == (x_2, y_2) {
+                continue;
+            }
+            antinodes.insert((*x, *y));
+            let dist = (x_2 - x, y_2 - y);
+
+            let mut antinode = (*x_2, *y_2);
+            while is_in_bounds(antinode.0, antinode.1) {
+                antinodes.insert(antinode);
+                antinode = (antinode.0 + dist.0, antinode.1 + dist.1);
+            }
+
+            let mut antinode = (*x_2, *y_2);
+            while is_in_bounds(antinode.0, antinode.1) {
+                antinodes.insert(antinode);
+                antinode = (antinode.0 - dist.0, antinode.1 - dist.1);
+            }
+        }
+    }
+
+    Some(antinodes.len() as u32)
 }
 
 #[cfg(test)]
@@ -58,6 +101,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(34));
     }
 }
