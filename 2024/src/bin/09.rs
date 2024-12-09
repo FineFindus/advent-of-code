2a22a571f534b8028754c20e40112593cc9f4b56
FineFindus <FineFindus@proton.me>
2024-12-09 22:20:30 +0100

    feat(2024/09): solve part two

--- 2024/src/bin/09.rs
+++ 2024/src/bin/09.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeMap;
+
 use itertools::Itertools;
 
 advent_of_code::solution!(9);
@@ -44,8 +46,55 @@ pub fn part_one(input: &str) -> Option<u64> {
         .sum1()
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut block_id = 0;
+    let (files, free_space) = input
+        .chars()
+        .filter_map(|char| char.to_digit(10))
+        .map(|size| size as u64)
+        .enumerate()
+        .fold(
+            (vec![], vec![]),
+            |(mut files, mut free_space), (idx, size)| {
+                if size > 0 {
+                    if idx % 2 == 0 {
+                        files.push((block_id, size));
+                    } else {
+                        free_space.push((block_id, size));
+                    }
+                    block_id += size;
+                }
+
+                (files, free_space)
+            },
+        );
+
+    let mut free_space_ids: BTreeMap<u64, u64> = BTreeMap::from_iter(free_space);
+    let mut checksum = 0;
+
+    for (block_id, &(file_block_id, file_size)) in files.iter().enumerate().rev() {
+        // check if there is any free space before our file
+        let free_space = free_space_ids
+            .iter()
+            .take_while(|&(&id, _size)| id < file_block_id)
+            .find(|&(_id, &block_size)| block_size >= file_size);
+
+        let new_file_block_id = if let Some((&free_space_id, &free_space_size)) = free_space {
+            free_space_ids.remove(&free_space_id);
+            // re-add left-over space
+            if free_space_size > file_size {
+                free_space_ids.insert(free_space_id + file_size, free_space_size - file_size);
+            }
+            free_space_id
+        } else {
+            file_block_id
+        };
+
+        // arithmetic sum, see https://pastebin.com/HrJPUAdq
+        checksum += (block_id as u64) * (new_file_block_id * 2 + file_size - 1) * file_size / 2;
+    }
+
+    checksum.into()
 }
 
 #[cfg(test)]
@@ -61,6 +110,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2858));
     }
 }
