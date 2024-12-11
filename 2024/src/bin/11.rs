b17e487aec36faed71b5bc926d83ef65663ec7bc
FineFindus <FineFindus@proton.me>
2024-12-11 20:40:39 +0100

    fix(2024/11): properly test second part

--- 2024/src/bin/11.rs
+++ 2024/src/bin/11.rs
@@ -63,6 +63,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(65601038650482));
     }
 }
