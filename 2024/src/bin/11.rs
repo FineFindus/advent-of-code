7d00032e9c03c7e04a328bcc6b15550667b22c53
FineFindus <FineFindus@proton.me>
2024-12-11 20:39:26 +0100

    refactor(2024/11): abstract commong blink logic

--- 2024/src/bin/11.rs
+++ 2024/src/bin/11.rs
@@ -4,7 +4,7 @@ use itertools::Itertools;
 
 advent_of_code::solution!(11);
 
-fn advance_stone(stone: u64, cache: &mut HashMap<(u64, usize), u64>, blinks: usize) -> u64 {
+fn blink_stone(stone: u64, cache: &mut HashMap<(u64, usize), u64>, blinks: usize) -> u64 {
     if blinks == 0 {
         return 1;
     }
@@ -18,13 +18,13 @@ fn advance_stone(stone: u64, cache: &mut HashMap<(u64, usize), u64>, blinks: usi
         0 => 1,
         n if digits(n) % 2 == 0 => {
             let divisor = 10u64.pow(digits(n) / 2);
-            sum += advance_stone(n / divisor, cache, blinks - 1);
+            sum += blink_stone(n / divisor, cache, blinks - 1);
             n % divisor
         }
         n => n * 2024,
     };
 
-    sum += advance_stone(blinked_engraving, cache, blinks - 1);
+    sum += blink_stone(blinked_engraving, cache, blinks - 1);
     cache.insert((stone, blinks), sum);
     sum
 }
@@ -33,22 +33,21 @@ fn digits(n: u64) -> u32 {
     n.ilog10() + 1
 }
 
-pub fn part_one(input: &str) -> Option<u64> {
+fn blink(input: &str, blinks: usize) -> Option<u64> {
     let mut cache = HashMap::new();
     input
         .split(" ")
         .map(|num| num.parse::<u64>().unwrap())
-        .map(|stone| advance_stone(stone, &mut cache, 25))
+        .map(|stone| blink_stone(stone, &mut cache, blinks))
         .sum1()
 }
 
+pub fn part_one(input: &str) -> Option<u64> {
+    blink(input, 25)
+}
+
 pub fn part_two(input: &str) -> Option<u64> {
-    let mut cache = HashMap::new();
-    input
-        .split(" ")
-        .map(|num| num.parse::<u64>().unwrap())
-        .map(|stone| advance_stone(stone, &mut cache, 75))
-        .sum1()
+    blink(input, 75)
 }
 
 #[cfg(test)]
