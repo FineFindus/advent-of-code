1b40c211ba74d378727355d34cde6029e282056a
FineFindus <FineFindus@proton.me>
2024-12-11 18:32:36 +0100

    feat(2024/11): solve part two

--- 2024/src/bin/11.rs
+++ 2024/src/bin/11.rs
@@ -1,40 +1,54 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 
 advent_of_code::solution!(11);
 
-fn advance_stone(stone: u128, blinks: usize) -> u128 {
+fn advance_stone(stone: u64, cache: &mut HashMap<(u64, usize), u64>, blinks: usize) -> u64 {
     if blinks == 0 {
         return 1;
+    }
+
+    if let Some(blinked_stones) = cache.get(&(stone, blinks)) {
+        return *blinked_stones;
     }
 
     let mut sum = 0;
     let blinked_engraving = match stone {
         0 => 1,
         n if digits(n) % 2 == 0 => {
-            let divisor = 10u128.pow(digits(n) / 2);
-            sum += advance_stone(n / divisor, blinks - 1);
+            let divisor = 10u64.pow(digits(n) / 2);
+            sum += advance_stone(n / divisor, cache, blinks - 1);
             n % divisor
         }
         n => n * 2024,
     };
 
-    advance_stone(blinked_engraving, blinks - 1) + sum
+    sum += advance_stone(blinked_engraving, cache, blinks - 1);
+    cache.insert((stone, blinks), sum);
+    sum
 }
 
-fn digits(n: u128) -> u32 {
+fn digits(n: u64) -> u32 {
     n.ilog10() + 1
 }
 
-pub fn part_one(input: &str) -> Option<u128> {
+pub fn part_one(input: &str) -> Option<u64> {
+    let mut cache = HashMap::new();
     input
         .split(" ")
-        .map(|num| num.parse::<u128>().unwrap())
-        .map(|stone| advance_stone(stone, 25))
+        .map(|num| num.parse::<u64>().unwrap())
+        .map(|stone| advance_stone(stone, &mut cache, 25))
         .sum1()
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut cache = HashMap::new();
+    input
+        .split(" ")
+        .map(|num| num.parse::<u64>().unwrap())
+        .map(|stone| advance_stone(stone, &mut cache, 75))
+        .sum1()
 }
 
 #[cfg(test)]
