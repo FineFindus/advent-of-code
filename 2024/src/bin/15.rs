efa9133f836ef47b72393583d06cf96a985ec0f2
FineFindus <FineFindus@proton.me>
2024-12-15 14:31:04 +0100

    feat(2024/15): solve part two

--- 2024/src/bin/15.rs
+++ 2024/src/bin/15.rs
@@ -77,8 +77,121 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(gps_sum as u32)
 }
 
+fn move_entity(
+    grid: &mut [Vec<char>],
+    entity: char,
+    postion: (i32, i32),
+    direction: (i32, i32),
+    tries_left: usize,
+) -> Option<(i32, i32)> {
+    if tries_left == 0 {
+        return None;
+    }
+    let target_pos = (postion.0 + direction.0, postion.1 + direction.1);
+    match cell(grid, target_pos.0, target_pos.1) {
+        Some(&'#') => None,
+        Some(&'.') => {
+            grid[postion.0 as usize][postion.1 as usize] = '.';
+            grid[target_pos.0 as usize][target_pos.1 as usize] = entity;
+            Some(target_pos)
+        }
+        Some(&'[') => {
+            if move_entity(
+                grid,
+                ']',
+                (target_pos.0, target_pos.1 + 1),
+                direction,
+                tries_left - 1,
+            )
+            .is_some()
+                && move_entity(grid, '[', target_pos, direction, tries_left - 1).is_some()
+            {
+                grid[postion.0 as usize][postion.1 as usize] = '.';
+                if entity == '[' {
+                    grid[postion.0 as usize][(postion.1 + 1) as usize] = '.';
+                }
+                grid[target_pos.0 as usize][target_pos.1 as usize] = entity;
+                Some(target_pos)
+            } else {
+                None
+            }
+        }
+        Some(&']') => {
+            if move_entity(
+                grid,
+                '[',
+                (target_pos.0, target_pos.1 - 1),
+                direction,
+                tries_left - 1,
+            )
+            .is_some()
+                && move_entity(grid, ']', target_pos, direction, tries_left - 1).is_some()
+            {
+                grid[postion.0 as usize][postion.1 as usize] = '.';
+                if entity == ']' {
+                    grid[postion.0 as usize][(postion.1 - 1) as usize] = '.';
+                }
+                grid[target_pos.0 as usize][target_pos.1 as usize] = entity;
+                Some(target_pos)
+            } else {
+                None
+            }
+        }
+        Some(_) | None => None,
+    }
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (grid, moves) = input.split_once("\n\n")?;
+    let mut grid = grid
+        .lines()
+        .map(|line| {
+            line.chars()
+                .flat_map(|char| match char {
+                    '#' => ['#', '#'],
+                    '.' => ['.', '.'],
+                    'O' => ['[', ']'],
+                    '@' => ['@', '.'],
+                    _ => unreachable!("Unexpected {char}"),
+                })
+                .collect_vec()
+        })
+        .collect_vec();
+
+    let moves = moves.lines().flat_map(|line| line.chars()).collect_vec();
+
+    let mut robot = grid
+        .iter()
+        .enumerate()
+        .find_map(|(idx, row)| Some((idx as i32, row.iter().position(|char| char == &'@')? as i32)))
+        .expect("robot should be in grid");
+
+    for move_type in moves {
+        let reset_grid = grid.clone();
+        let direction = match move_type {
+            '<' => (0, -1),
+            '^' => (-1, 0),
+            '>' => (0, 1),
+            'v' => (1, 0),
+            v => unreachable!("Unkown move instruction {v}"),
+        };
+        if let Some(position) = move_entity(&mut grid, '@', robot, direction, 3000) {
+            robot = position;
+        } else {
+            grid = reset_grid;
+        }
+    }
+
+    let mut gps_sum = 0;
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            if grid[y][x] != '[' {
+                continue;
+            }
+            gps_sum += 100 * y + x;
+        }
+    }
+    Some(gps_sum as u32)
 }
 
 #[cfg(test)]
@@ -94,6 +207,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(9021));
     }
 }
