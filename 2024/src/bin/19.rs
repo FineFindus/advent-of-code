313ab95a72edcd1c2cb5a25788b2f121dae5c54d
FineFindus <FineFindus@proton.me>
2024-12-19 14:15:46 +0100

    feat(2024/19): solve part two

--- 2024/src/bin/19.rs
+++ 2024/src/bin/19.rs
@@ -1,35 +1,53 @@
+use std::collections::HashMap;
+
+use itertools::Itertools;
+
 advent_of_code::solution!(19);
 
-fn is_design_possible(design: &str, towels: &mut Vec<&str>) -> bool {
+fn is_design_possible<'a>(
+    design: &'a str,
+    towels: &[&str],
+    cache: &mut HashMap<&'a str, u64>,
+) -> Option<u64> {
     if design.is_empty() {
-        return true;
+        return Some(1);
+    }
+    if let Some(combinations) = cache.get(design) {
+        return Some(*combinations);
     }
 
-    for index in 0..towels.len() {
-        let towel = towels[index];
-        if let Some(design) = design.strip_prefix(towel) {
-            if is_design_possible(design, towels) {
-                return true;
-            }
-        }
-    }
-
-    false
+    towels
+        .iter()
+        .filter_map(|towel| design.strip_prefix(towel))
+        .filter_map(|design| {
+            let combinations = is_design_possible(design, towels, cache)?;
+            cache.insert(design, combinations);
+            Some(combinations)
+        })
+        .sum1()
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let (towels, designs) = input.split_once("\n\n")?;
-    let mut towels: Vec<&str> = towels.split(", ").collect();
+    let towels: Vec<&str> = towels.split(", ").collect();
 
+    let mut cache = HashMap::new();
     let possible_designs = designs
         .lines()
-        .filter(|design| is_design_possible(design, &mut towels))
+        .filter(|design| is_design_possible(design, &towels, &mut cache).is_some())
         .count();
     Some(possible_designs as u32)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let (towels, designs) = input.split_once("\n\n")?;
+    let towels: Vec<&str> = towels.split(", ").collect();
+
+    let mut cache = HashMap::new();
+    designs
+        .lines()
+        .filter_map(|design| is_design_possible(design, &towels, &mut cache))
+        .sum1()
 }
 
 #[cfg(test)]
@@ -45,6 +63,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(16));
     }
 }
