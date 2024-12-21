4a1ac4fe9cc60d7474994cf0cfc13f86bc1fa074
FineFindus <FineFindus@proton.me>
2024-12-21 23:27:06 +0100

    refactor(2024/21): simplify code

--- 2024/src/bin/21.rs
+++ 2024/src/bin/21.rs
@@ -4,11 +4,10 @@ use itertools::Itertools;
 
 advent_of_code::solution!(21);
 
-fn find_numeric_path(grid: &[Vec<char>], start: (i32, i32), goal: (i32, i32)) -> Vec<char> {
+fn find_numeric_path(start: (i32, i32), goal: (i32, i32)) -> Vec<char> {
     let dy = goal.0 - start.0;
     let dx = goal.1 - start.1;
 
-    // let covers_empty_spot = (goal.0 == 0 && start.1 == -2) || (start.0 == 0 && goal.1 == -2);
     let mut directions = Vec::new();
     let horiz = Vec::from_iter(
         std::iter::repeat(if dx >= 0 { '>' } else { '<' }).take(dx.unsigned_abs() as usize),
@@ -33,11 +32,10 @@ fn find_numeric_path(grid: &[Vec<char>], start: (i32, i32), goal: (i32, i32)) ->
     }
     directions
 }
-fn find_dir_path(grid: &[Vec<char>], start: (i32, i32), goal: (i32, i32)) -> Vec<char> {
+fn find_dir_path(start: (i32, i32), goal: (i32, i32)) -> Vec<char> {
     let dy = goal.0 - start.0;
     let dx = goal.1 - start.1;
 
-    // let covers_empty_spot = (goal.0 == 0 && start.1 == -2) || (start.0 == 0 && goal.1 == -2);
     let mut directions = Vec::new();
     let horiz = Vec::from_iter(
         std::iter::repeat(if dx >= 0 { '>' } else { '<' }).take(dx.unsigned_abs() as usize),
@@ -63,27 +61,13 @@ fn find_dir_path(grid: &[Vec<char>], start: (i32, i32), goal: (i32, i32)) -> Vec
     directions
 }
 
-fn encode_numeric_keypad(keypad: &[Vec<char>], start: (i32, i32), keys: &[char]) -> Vec<char> {
-    // Directional map with (i32, i32) coordinates
-    let mut numerical_map: HashMap<char, (i32, i32)> = HashMap::new();
-    numerical_map.insert('A', (0, 2));
-    numerical_map.insert('0', (0, 1));
-    numerical_map.insert('1', (1, 0));
-    numerical_map.insert('2', (1, 1));
-    numerical_map.insert('3', (1, 2));
-    numerical_map.insert('4', (2, 0));
-    numerical_map.insert('5', (2, 1));
-    numerical_map.insert('6', (2, 2));
-    numerical_map.insert('7', (3, 0));
-    numerical_map.insert('8', (3, 1));
-    numerical_map.insert('9', (3, 2));
-
-    let mut position = numerical_map[&'A'];
+fn encode_numeric_keypad(keypad: &HashMap<char, (i32, i32)>, keys: &[char]) -> Vec<char> {
+    let mut position = keypad[&'A'];
     let mut directions = Vec::new();
     for key in keys {
-        let goal = numerical_map[key];
+        let goal = keypad[key];
         if position != goal {
-            let path = find_numeric_path(keypad, position, goal);
+            let path = find_numeric_path(position, goal);
             directions.extend(path);
         }
 
@@ -93,19 +77,13 @@ fn encode_numeric_keypad(keypad: &[Vec<char>], start: (i32, i32), keys: &[char])
     directions
 }
 
-fn encode_keypad(keypad: &[Vec<char>], start: (i32, i32), keys: &[char]) -> Vec<char> {
-    let mut directional_map: HashMap<char, (i32, i32)> = HashMap::new();
-    directional_map.insert('A', (1, 2));
-    directional_map.insert('^', (1, 1));
-    directional_map.insert('<', (0, 0));
-    directional_map.insert('v', (0, 1));
-    directional_map.insert('>', (0, 2));
-    let mut position = directional_map[&'A'];
+fn encode_keypad(keypad: &HashMap<char, (i32, i32)>, keys: &[char]) -> Vec<char> {
+    let mut position = keypad[&'A'];
     let mut directions = Vec::new();
     for key in keys {
-        let goal = directional_map[key];
+        let goal = keypad[key];
         if position != goal {
-            let path = find_dir_path(keypad, position, goal);
+            let path = find_dir_path(position, goal);
             directions.extend(path);
         }
         directions.push('A');
@@ -127,40 +105,47 @@ fn numeric_value(chars: &[char]) -> u32 {
     result
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+fn compute_complexity(input: &str) -> Option<u32> {
     let keycodes = input
         .lines()
         .map(|line| line.chars().collect_vec())
         .collect_vec();
 
-    let numeric_keypad = [
-        vec!['7', '8', '9'],
-        vec!['4', '5', '6'],
-        vec!['1', '2', '3'],
-        vec![' ', '0', 'A'],
-    ];
-    const NUMERIC_KEYPAD_A: (i32, i32) = (3, 2);
+    let numeric_keypad = HashMap::from([
+        ('A', (0, 2)),
+        ('0', (0, 1)),
+        ('1', (1, 0)),
+        ('2', (1, 1)),
+        ('3', (1, 2)),
+        ('4', (2, 0)),
+        ('5', (2, 1)),
+        ('6', (2, 2)),
+        ('7', (3, 0)),
+        ('8', (3, 1)),
+        ('9', (3, 2)),
+    ]);
 
-    let directional_keypad = [vec![' ', '^', 'A'], vec!['<', 'v', '>']];
-    const DIRECTIONAL_KEYPAD_A: (i32, i32) = (0, 2);
+    let directional_keypad = HashMap::from([
+        ('A', (1, 2)),
+        ('^', (1, 1)),
+        ('<', (0, 0)),
+        ('v', (0, 1)),
+        ('>', (0, 2)),
+    ]);
+
     let mut complexity = 0;
     for keys in keycodes {
-        let numeric_input = encode_numeric_keypad(&numeric_keypad, NUMERIC_KEYPAD_A, &keys);
-        dbg!(numeric_input.iter().join(""));
-        let robot_directional_input =
-            encode_keypad(&directional_keypad, DIRECTIONAL_KEYPAD_A, &numeric_input);
-        dbg!(robot_directional_input.iter().join(""));
-        let directional_input = encode_keypad(
-            &directional_keypad,
-            DIRECTIONAL_KEYPAD_A,
-            &robot_directional_input,
-        );
-        dbg!(directional_input.iter().join(""));
+        let door_sequence = encode_numeric_keypad(&numeric_keypad, &keys);
+        let robot_directional_input = encode_keypad(&directional_keypad, &door_sequence);
+        let directional_input = encode_keypad(&directional_keypad, &robot_directional_input);
         let length = directional_input.len() as u32;
-        dbg!(length);
         complexity += numeric_value(&keys[..3]) * length;
     }
     Some(complexity)
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    compute_complexity(input)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
