cace8e1b2c1c12732c53bf603d1944b6e51d4f6f
FineFindus <FineFindus@proton.me>
2024-12-23 21:48:15 +0100

    feat(2024/23): solve part two

--- 2024/src/bin/23.rs
+++ 2024/src/bin/23.rs
@@ -1,6 +1,42 @@
 use std::collections::{HashMap, HashSet};
 
+use itertools::Itertools;
+
 advent_of_code::solution!(23);
+
+fn bron_kerbosch2<'a>(
+    graph: &HashMap<&'a str, HashSet<&'a str>>,
+    cliques: &mut Vec<HashSet<&'a str>>,
+    r: &mut HashSet<&'a str>,
+    p: &mut HashSet<&'a str>,
+    x: &mut HashSet<&'a str>,
+) {
+    // algorithm BronKerbosch2(R, P, X) is
+    if p.is_empty() {
+        if x.is_empty() {
+            cliques.push(r.clone());
+        }
+        return;
+    }
+    // choose a pivot vertex u in P ⋃ X
+    let pivot = p.union(x).copied().next().unwrap();
+    let p_without_neighbors: HashSet<_> = p.difference(&graph[&pivot]).copied().collect();
+    // for each vertex v in P \ N(u) do
+    for v in p_without_neighbors {
+        let neighbors = &graph[v];
+        // BronKerbosch2(R ⋃ {v}, P ⋂ N(v), X ⋂ N(v))
+        let mut p_next = p.intersection(neighbors).copied().collect();
+        let mut x_next = x.intersection(neighbors).copied().collect();
+        r.insert(v);
+        bron_kerbosch2(graph, cliques, r, &mut p_next, &mut x_next);
+        r.remove(v);
+        // P := P \ {v}
+        p.remove(v);
+        // X := X ⋃ {v}
+        x.insert(v);
+    }
+}
+
 fn find_triangles<'a>(graph: &HashMap<&'a str, HashSet<&'a str>>) -> Vec<Vec<&'a str>> {
     let mut triangles = Vec::new();
 
@@ -22,24 +58,19 @@ fn find_triangles<'a>(graph: &HashMap<&'a str, HashSet<&'a str>>) -> Vec<Vec<&'a
     triangles
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let graph = input.lines().filter_map(|line| line.split_once("-")).fold(
-        HashMap::new(),
-        |mut acc, (a, b)| {
-            acc.entry(a)
-                .and_modify(|connections: &mut HashSet<&str>| {
-                    connections.insert(b);
-                })
-                .or_insert(HashSet::from([b]));
-            acc.entry(b)
-                .and_modify(|connections: &mut HashSet<&str>| {
-                    connections.insert(a);
-                })
-                .or_insert(HashSet::from([a]));
+fn parse_graph(input: &str) -> HashMap<&str, HashSet<&str>> {
+    input
+        .lines()
+        .filter_map(|line| line.split_once("-"))
+        .fold(HashMap::new(), |mut acc, (a, b)| {
+            acc.entry(a).or_default().insert(b);
+            acc.entry(b).or_default().insert(a);
             acc
-        },
-    );
-    // find_cliques(&graph);
+        })
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let graph = parse_graph(input);
     let connected_computer = find_triangles(&graph)
         .iter()
         .filter(|triangle| triangle.iter().any(|v| v.starts_with("t")))
@@ -47,8 +78,18 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(connected_computer as u32)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<String> {
+    let graph = parse_graph(input);
+
+    let mut cliques = Vec::new();
+    let mut p: HashSet<&str> = graph.keys().copied().collect();
+    let mut r = HashSet::new();
+    let mut x = HashSet::new();
+    bron_kerbosch2(&graph, &mut cliques, &mut r, &mut p, &mut x);
+    cliques
+        .iter()
+        .max_by_key(|v| v.len())
+        .map(|max| max.iter().sorted().join(","))
 }
 
 #[cfg(test)]
@@ -64,6 +105,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some("co,de,ka,ta".to_string()));
     }
 }
