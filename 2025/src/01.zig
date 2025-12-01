ecc47c1f53a3885cf568a3adea5912df109c2c70
FineFindus <FineFindus@proton.me>
2025-12-01 13:49:55 +0100

    feat(2025/01): solve part two

--- 2025/src/01.zig
+++ 2025/src/01.zig
@@ -24,8 +24,28 @@ pub fn part1(input: []const u8, _: std.mem.Allocator) !usize {
     return counter;
 }
 
-pub fn part2(_: []const u8, _: std.mem.Allocator) !usize {
-    return 0;
+pub fn part2(input: []const u8, _: std.mem.Allocator) !usize {
+    var counter: usize = 0;
+    var dial: isize = 50;
+    var it = std.mem.splitSequence(u8, input, "\n");
+
+    while (it.next()) |line| {
+        if (line.len == 0) break;
+
+        const direction = line[0];
+        const rotations = try std.fmt.parseInt(usize, line[1..], 10);
+
+        for (0..rotations) |_| {
+            dial = switch (direction) {
+                'L' => dial -% 1,
+                'R' => dial + 1,
+                else => dial,
+            };
+            dial = @mod(dial, 100);
+            if (dial == 0) counter += 1;
+        }
+    }
+    return counter;
 }
 
 pub fn main() !void {
@@ -44,5 +64,5 @@ test "part one" {
 
 test "part two" {
     const gpa = std.testing.allocator;
-    try std.testing.expectEqual(0, part2(example, gpa));
+    try std.testing.expectEqual(6, part2(example, gpa));
 }
