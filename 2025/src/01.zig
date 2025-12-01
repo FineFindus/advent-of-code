e5a34c3a3914726ec2ebc4475133131d735aefa6
FineFindus <FineFindus@proton.me>
2025-12-01 09:11:07 +0100

    refactor(2025/01): simplify part one

--- 2025/src/01.zig
+++ 2025/src/01.zig
@@ -7,24 +7,19 @@ const data = @embedFile("data/inputs/01.txt");
 pub fn part1(input: []const u8, _: std.mem.Allocator) !usize {
     var counter: usize = 0;
     var dial: isize = 50;
-    var it = std.mem.splitScalar(u8, input, '\n');
-    while (it.next()) |val| {
-        if (val.len == 0) {
-            break;
-        }
-        const rotations: isize = @intCast(try std.fmt.parseInt(i32, val[1..], 10));
-        switch (val[0]) {
-            @as(u8, 'L') => {
-                dial = @mod(dial -% rotations, 100);
-            },
-            @as(u8, 'R') => {
-                dial = @mod(dial +% rotations, 100);
-            },
-            else => {},
-        }
-        if (dial == 0) {
-            counter += 1;
-        }
+    var it = std.mem.splitSequence(u8, input, "\n");
+
+    while (it.next()) |line| {
+        if (line.len == 0) break;
+
+        const rotations = try std.fmt.parseInt(i32, line[1..], 10);
+        const direction = line[0];
+        dial = switch (direction) {
+            'L' => @mod(dial -% rotations, 100),
+            'R' => @mod(dial +% rotations, 100),
+            else => dial,
+        };
+        if (dial == 0) counter += 1;
     }
     return counter;
 }
