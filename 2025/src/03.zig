92e05288fcd0935f22c1804196fe1fbad39c9488
FineFindus <FineFindus@proton.me>
2025-12-03 09:55:55 +0100

    feat(2025/03): solve part one

---
+++ 2025/src/03.zig
@@ -0,0 +1,60 @@
+const std = @import("std");
+const aoc = @import("advent-of-code");
+
+const example = @embedFile("data/examples/03.txt");
+
+pub fn part1(input: []const u8, _: std.mem.Allocator) !usize {
+    var sum: usize = 0;
+
+    var it = std.mem.splitSequence(u8, input, "\n");
+    while (it.next()) |line| {
+        if (line.len == 0) break;
+        var first: usize = 0;
+        var second: usize = 0;
+
+        for (0..(line.len - 1)) |index| {
+            const value = line[index] - '0';
+
+            if (value > first) {
+                first = value;
+                second = line[index + 1] - '0';
+            } else if (value > second) {
+                second = value;
+            }
+        }
+        // special case the last char, we cannot pick it as the first value
+        const last = line[line.len - 1] - '0';
+        if (last > second) {
+            second = last;
+        }
+
+        sum += (first * 10) + second;
+    }
+
+    return sum;
+}
+
+pub fn part2(_: []const u8, _: std.mem.Allocator) !usize {
+    return 0;
+}
+
+pub fn main() !void {
+    const input = @embedFile("data/inputs/03.txt");
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    try aoc.solve(input, 1, part1, allocator);
+    try aoc.solve(input, 2, part2, allocator);
+}
+
+test "part one" {
+    const gpa = std.testing.allocator;
+    try std.testing.expectEqual(357, part1(example, gpa));
+}
+
+test "part two" {
+    const gpa = std.testing.allocator;
+    try std.testing.expectEqual(0, part2(example, gpa));
+}
