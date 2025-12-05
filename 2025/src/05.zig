20eb5cfcc76797a447d171dd7a021f4201c7710c
FineFindus <FineFindus@proton.me>
2025-12-05 21:22:46 +0100

    refactor(2025/05): extract common parsing code

--- 2025/src/05.zig
+++ 2025/src/05.zig
@@ -25,12 +25,10 @@ const Interval = struct {
     }
 };
 
-pub fn part1(input: []const u8, allocator: std.mem.Allocator) !usize {
-    var input_it = std.mem.splitSequence(u8, input, "\n\n");
-    var fresh_ingredients_it = std.mem.splitSequence(u8, input_it.next().?, "\n");
+fn parse_fresh_ingredients(input: []const u8, allocator: std.mem.Allocator) !std.ArrayList(Interval) {
+    var fresh_ingredients_it = std.mem.splitSequence(u8, input, "\n");
 
-    var fresh_ingredients = try std.ArrayList(Interval).initCapacity(allocator, 1024);
-    defer fresh_ingredients.deinit(allocator);
+    var fresh_ingredients = try std.ArrayList(Interval).initCapacity(allocator, 256);
 
     while (fresh_ingredients_it.next()) |line| {
         if (line.len == 0) break;
@@ -38,13 +36,22 @@ pub fn part1(input: []const u8, allocator: std.mem.Allocator) !usize {
         var it = std.mem.splitSequence(u8, line, "-");
         const lower = try std.fmt.parseInt(usize, it.next().?, 10);
         const upper = try std.fmt.parseInt(usize, it.next().?, 10);
+
         try fresh_ingredients.append(allocator, Interval{ .lower = lower, .upper = upper });
     }
+
+    return fresh_ingredients;
+}
+
+pub fn part1(input: []const u8, allocator: std.mem.Allocator) !usize {
+    var it = std.mem.splitSequence(u8, input, "\n\n");
+    var fresh_ingredients = try parse_fresh_ingredients(it.next().?, allocator);
+    defer fresh_ingredients.deinit(allocator);
 
     std.sort.pdq(Interval, fresh_ingredients.items, {}, Interval.sortFn);
 
     var sum: usize = 0;
-    var available_ingredients = std.mem.splitSequence(u8, input_it.next().?, "\n");
+    var available_ingredients = std.mem.splitSequence(u8, it.next().?, "\n");
     while (available_ingredients.next()) |line| {
         if (line.len == 0) break;
         const id = try std.fmt.parseInt(usize, line, 10);
@@ -62,21 +69,9 @@ pub fn part1(input: []const u8, allocator: std.mem.Allocator) !usize {
 }
 
 pub fn part2(input: []const u8, allocator: std.mem.Allocator) !usize {
-    var input_it = std.mem.splitSequence(u8, input, "\n\n");
-    var fresh_ingredients_it = std.mem.splitSequence(u8, input_it.next().?, "\n");
-
-    var fresh_ingredients = try std.ArrayList(Interval).initCapacity(allocator, 256);
+    var it = std.mem.splitSequence(u8, input, "\n\n");
+    var fresh_ingredients = try parse_fresh_ingredients(it.next().?, allocator);
     defer fresh_ingredients.deinit(allocator);
-
-    while (fresh_ingredients_it.next()) |line| {
-        if (line.len == 0) break;
-
-        var it = std.mem.splitSequence(u8, line, "-");
-        const lower = try std.fmt.parseInt(usize, it.next().?, 10);
-        const upper = try std.fmt.parseInt(usize, it.next().?, 10);
-
-        try fresh_ingredients.append(allocator, Interval{ .lower = lower, .upper = upper });
-    }
 
     std.sort.pdq(Interval, fresh_ingredients.items, {}, Interval.sortFn);
 
