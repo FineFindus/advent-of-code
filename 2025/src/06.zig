484790647cb627e50cdaebde52bd4dbc63fa3371
FineFindus <FineFindus@proton.me>
2025-12-06 22:02:03 +0100

    perf(2025/06): avoid list allocation for part one
    Instead of first parsing and then calculating, we can parse and calculate in
    one pass, remove the need to store the parsed numbers, improving performance.

--- 2025/src/06.zig
+++ 2025/src/06.zig
@@ -3,57 +3,38 @@ const aoc = @import("advent-of-code");
 
 const example = @embedFile("data/examples/06.txt");
 
-pub fn part1(input: []const u8, allocator: std.mem.Allocator) !usize {
-    var operands = try std.ArrayList(usize).initCapacity(allocator, 4888);
-    defer operands.deinit(allocator);
+pub fn part1(input: []const u8, _: std.mem.Allocator) !usize {
+    const line_length = std.mem.indexOfPos(u8, input, 0, "\n").?;
+    // all lines have the same length
+    const lines = input.len / line_length;
 
-    var index: usize = 0;
-    var it = std.mem.splitSequence(u8, input, "\n");
-    var line_length: ?usize = null;
-    while (it.next()) |line| {
-        var position: usize = 0;
-        while (position < line.len - 2) {
+    var sum: usize = 0;
+    var column: usize = 0;
+    while (column < line_length) {
+        const symbol = input[(lines - 1) * (line_length + 1) + column];
+        var intermediate_result: usize = if (symbol == '*') 1 else 0;
+
+        var num_len: usize = 0;
+        for (0..lines - 1) |row| {
+            const line = input[row * (line_length + 1) + column ..];
+
+            var position: usize = 0;
             while (line[position] == ' ') : (position += 1) {}
 
             var next_empty = position + 1;
-            while (next_empty < line.len and line[next_empty] != ' ') : (next_empty += 1) {}
+            while (next_empty < line.len and (line[next_empty] != ' ' and line[next_empty] != '\n')) : (next_empty += 1) {}
+            num_len = @max(num_len, next_empty);
 
             const value = try std.fmt.parseInt(usize, line[position..next_empty], 10);
-            try operands.append(allocator, value);
 
-            position = next_empty;
-        }
-
-        if (line_length == null) line_length = operands.items.len;
-        index += 1;
-
-        const peek = it.peek().?;
-        // next line is the operands
-        if (peek[0] == '*' or peek[0] == '+') break;
-    }
-
-    var sum: usize = 0;
-
-    const line = it.next().?;
-    var position: usize = 0;
-    var column: usize = 0;
-    outer: while (position < line.len) : (position += 1) {
-        while (line[position] == ' ') {
-            if (position == line.len - 1) break :outer;
-            position += 1;
-        }
-
-        var result: usize = if (line[position] == '*') 1 else 0;
-        for (0..index) |i| {
-            const value = operands.items[i * line_length.? + column];
-            switch (line[position]) {
-                '*' => result *= value,
-                '+' => result += value,
-                else => {},
+            if (symbol == '*') {
+                intermediate_result *= value;
+            } else {
+                intermediate_result += value;
             }
         }
-        column += 1;
-        sum += result;
+        column += num_len + 1;
+        sum += intermediate_result;
     }
 
     return sum;
