f368b23af1ddb4a181a589c74b00e5eda3acb04d
FineFindus <FineFindus@proton.me>
2025-12-07 21:23:58 +0100

    perf(2025/07): use simple iteration
    Instead of performing BFS on the input, we can simply iterate through the input
    grid, storing whether we have a beam at that column. This speeds up the
    computation massively.
    
    Ref: https://github.com/ropewalker/advent_of_code_2025/blob/ab6e92d28d4f71ea6f6a4bd6d8c6b25c9f341771/src/day07.rs

--- 2025/src/07.zig
+++ 2025/src/07.zig
@@ -3,90 +3,32 @@ const aoc = @import("advent-of-code");
 
 const example = @embedFile("data/examples/07.txt");
 
-// from https://ziglang.org/learn/samples/
-pub fn Queue(comptime Child: type) type {
-    return struct {
-        const Node = struct {
-            data: Child,
-            next: ?*Node,
-        };
-        gpa: std.mem.Allocator,
-        start: ?*Node,
-        end: ?*Node,
-
-        pub fn init(gpa: std.mem.Allocator) @This() {
-            return @This(){
-                .gpa = gpa,
-                .start = null,
-                .end = null,
-            };
-        }
-
-        pub fn enqueue(self: *@This(), value: Child) !void {
-            const node = try self.gpa.create(Node);
-            node.* = .{ .data = value, .next = null };
-            if (self.end) |end| end.next = node //
-            else self.start = node;
-            self.end = node;
-        }
-
-        pub fn dequeue(self: *@This()) ?Child {
-            const start = self.start orelse return null;
-            defer self.gpa.destroy(start);
-
-            if (start.next) |next|
-                self.start = next
-            else {
-                self.start = null;
-                self.end = null;
-            }
-            return start.data;
-        }
-    };
-}
-
-pub fn part1(input: []const u8, allocator: std.mem.Allocator) !usize {
-    const line_length = std.mem.indexOfPos(u8, input, 0, "\n").?;
-
+pub fn part1(input: []const u8, _: std.mem.Allocator) !usize {
     const source_position = std.mem.indexOfPos(u8, input, 0, "S").?;
-    var visited = std.AutoHashMap(usize, void).init(allocator);
-    defer visited.deinit();
+    // input has a size of 155 lines
+    var rows: u256 = std.math.shl(u256, 1, source_position);
+    var splits: usize = 0;
 
-    var splitters = std.AutoHashMap(usize, void).init(allocator);
-    defer splitters.deinit();
+    var it = std.mem.splitSequence(u8, input, "\n");
+    // skip inital source line, we already handled it
+    _ = it.next();
+    while (it.next()) |line| {
+        if (line.len == 0) break;
 
-    var queue = Queue(usize).init(allocator);
-    try queue.enqueue(source_position);
+        for (line, 0..) |char, i| {
+            if (char == '^') {
+                const mask = std.math.shl(u256, 1, i);
+                if (rows & mask != 0) {
+                    splits += 1;
+                }
 
-    outer: while (queue.dequeue()) |beam| {
-        var position = beam;
-
-        // find next splitter
-        while (true) {
-            position += line_length + 1;
-            if (position >= input.len) continue :outer;
-            if (input[position] == '^') break;
-        }
-
-        // only count new splitters
-        if (!splitters.contains(position)) {
-            try splitters.put(position, {});
-        }
-
-        const left_pos = position - 1;
-        const right_pos = position + 1;
-
-        if (!visited.contains(left_pos)) {
-            try visited.put(left_pos, {});
-            try queue.enqueue(left_pos);
-        }
-
-        if (!visited.contains(right_pos)) {
-            try visited.put(right_pos, {});
-            try queue.enqueue(right_pos);
+                rows &= ~mask;
+                rows |= (mask << 1);
+                rows |= (mask >> 1);
+            }
         }
     }
-    return splitters.count();
+    return splits;
 }
 
 fn countTimelines(input: []const u8, source: usize, line_length: usize, cache: *std.AutoHashMap(usize, usize)) !usize {
