91536ca158d5bc92ba9f7b95052b3d2e5af43d9a
FineFindus <FineFindus@proton.me>
2025-12-11 16:53:04 +0100

    feat(2025/11): solve part two

--- 2025/src/11.zig
+++ 2025/src/11.zig
@@ -1,18 +1,26 @@
 const std = @import("std");
 const aoc = @import("advent-of-code");
 
-const example = @embedFile("data/examples/11.txt");
+const out = ((@as(u24, 'o') << 16) | (@as(u24, 'u') << 8) | 't');
 
 fn countPaths(
     source: u24,
     target: u24,
     graph: *const std.AutoArrayHashMap(u24, std.ArrayList(u24)),
     visited: *std.AutoHashMap(u24, void),
+    cache: *std.AutoHashMap(u24, usize),
     count: usize,
 ) !usize {
+    if (cache.get(source)) |cached_value| {
+        return cached_value;
+    }
+
     if (source == target) {
         return count + 1;
     }
+
+    // out does not have any out-going edges
+    if (source == out) return 0;
 
     // mark current node as visited
     try visited.put(source, {});
@@ -20,11 +28,12 @@ fn countPaths(
 
     for (graph.get(source).?.items) |node| {
         if (!visited.contains(node)) {
-            sum += try countPaths(node, target, graph, visited, count);
+            sum += try countPaths(node, target, graph, visited, cache, count);
         }
     }
 
     _ = visited.remove(source);
+    try cache.put(source, sum);
 
     return sum;
 }
@@ -57,14 +66,59 @@ pub fn part1(input: []const u8, allocator: std.mem.Allocator) !usize {
     defer visited.deinit();
 
     const source = ((@as(u24, 'y') << 16) | (@as(u24, 'o') << 8) | 'u');
-    const target = ((@as(u24, 'o') << 16) | (@as(u24, 'u') << 8) | 't');
-    const paths = try countPaths(source, target, &graph, &visited, 0);
+
+    var cache = std.AutoHashMap(u24, usize).init(allocator);
+    defer cache.deinit();
+    const paths = try countPaths(source, out, &graph, &visited, &cache, 0);
 
     return paths;
 }
 
-pub fn part2(_: []const u8, _: std.mem.Allocator) !usize {
-    return 0;
+pub fn part2(input: []const u8, allocator: std.mem.Allocator) !usize {
+    var graph = std.AutoArrayHashMap(u24, std.ArrayList(u24)).init(allocator);
+    defer {
+        var outputs_it = graph.iterator();
+        while (outputs_it.next()) |entry| {
+            entry.value_ptr.deinit(allocator);
+        }
+        graph.deinit();
+    }
+
+    var it = std.mem.splitSequence(u8, input[0 .. input.len - 1], "\n");
+    while (it.next()) |line| {
+        var options = try std.ArrayList(u24).initCapacity(allocator, 8);
+
+        var options_it = std.mem.splitSequence(u8, line[5..], " ");
+        while (options_it.next()) |option| {
+            const value = ((@as(u24, option[0]) << 16) | (@as(u24, option[1]) << 8) | option[2]);
+            try options.append(allocator, value);
+        }
+
+        const start = ((@as(u24, line[0]) << 16) | (@as(u24, line[1]) << 8) | line[2]);
+        try graph.put(start, options);
+    }
+
+    var visited = std.AutoHashMap(u24, void).init(allocator);
+    defer visited.deinit();
+
+    var cache = std.AutoHashMap(u24, usize).init(allocator);
+    defer cache.deinit();
+
+    const source = ((@as(u24, 's') << 16) | (@as(u24, 'v') << 8) | 'r');
+    const fft = ((@as(u24, 'f') << 16) | (@as(u24, 'f') << 8) | 't');
+    const dac = ((@as(u24, 'd') << 16) | (@as(u24, 'a') << 8) | 'c');
+
+    var paths = try countPaths(source, fft, &graph, &visited, &cache, 0);
+
+    cache.clearRetainingCapacity();
+    visited.clearRetainingCapacity();
+    paths *= try countPaths(fft, dac, &graph, &visited, &cache, 0);
+
+    cache.clearRetainingCapacity();
+    visited.clearRetainingCapacity();
+    paths *= try countPaths(dac, out, &graph, &visited, &cache, 0);
+
+    return paths;
 }
 
 pub fn main() !void {
@@ -79,11 +133,15 @@ pub fn main() !void {
 }
 
 test "part one" {
+    const example = @embedFile("data/examples/11.txt");
+
     const gpa = std.testing.allocator;
     try std.testing.expectEqual(5, part1(example, gpa));
 }
 
 test "part two" {
+    const example = @embedFile("data/examples/11_2.txt");
+
     const gpa = std.testing.allocator;
-    try std.testing.expectEqual(0, part2(example, gpa));
+    try std.testing.expectEqual(2, part2(example, gpa));
 }
